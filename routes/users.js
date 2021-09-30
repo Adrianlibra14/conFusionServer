838435325fd64b8dd89d5b8575c885e11c62d572
AdrianLibra14 <adrianjaa10142002@gmail.com>
2021-09-30 09:22:23 -0400

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -24,13 +24,29 @@ router.post('/signup', (req, res, next) => {
           err: err
         });
       } else {
-        // Esta sintaxis es así ya que el móulo de passport espera que se use así (Con el segundo parentesis, después de 'local') y representa una función
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({
-            success: true,
-            status: 'Registration Successful!',
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              err: err
+            });
+            return;
+          }
+          // Esta sintaxis es así ya que el móulo de passport espera que se use así (Con el segundo parentesis, después de 'local') y representa una función
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              success: true,
+              status: 'Registration Successful!',
+            });
           });
         });
       }
@@ -39,7 +55,9 @@ router.post('/signup', (req, res, next) => {
 
 // Esto crea una sesión y expende un JWT en el header
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  var token = authenticate.getToken({_id: req.user._id});
+  var token = authenticate.getToken({
+    _id: req.user._id
+  });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({
@@ -47,7 +65,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
     token: token,
     status: 'You are successfully logged in!',
   });
-}); 
+});
 
 router.get('/logout', (req, res, next) => {
   if (req.session) {
